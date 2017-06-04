47ae172753102a4ee9323b6983b53399f77e5f19
kaley.laporta <kaley.laporta@gmail.com>
2017-06-04 17:11:50 -0500

    Add player objects, render their paddles, create ball object

--- pong.js
+++ pong.js
@@ -37,3 +37,53 @@ var render = function() {
 	context.fillRect(0, 0, width, height);
 };
 
+// create paddle object
+function Paddle(x, y, width, height) {
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	this.x_speed = 0;
+	this.y_speed = 0;
+}
+
+Paddle.prototype.render = function() {
+	context.fillstyle = "#1eeded";
+	context.fillRect(this.x, this.y, this.width, this.height);
+};
+
+// create players, with Paddle objects - one will be the computer
+function Player() {
+	this.paddle = new Paddle(175, 580, 50, 10);
+}
+
+function Computer() {
+	this.paddle = new Paddle(175, 10, 50, 10)
+}
+
+// when rendering players - render their paddles
+Player.prototype.render = function() {
+	this.paddle.render();
+};
+
+Computer.prototype.render = function () {
+	this.paddle.render();
+};
+
+// create ball object, radius of 5, positions based on x,y coordinates
+function Ball(x, y) {
+	this.x = x;
+	this.y = y;
+	this.x_speed = 0;
+	this.y_speed = 3;
+	this.radius = 5;
+}
+
+
+
+
+
+
+
+
+
