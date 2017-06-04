d0f6a4059b46c5b6145869cedcbc91b9f804d682
kaley.laporta <kaley.laporta@gmail.com>
2017-06-04 17:37:27 -0500

    Add collision detection in Ball prototype update function

--- pong.js
+++ pong.js
@@ -28,8 +28,9 @@ var step = function() {
 	animate(step);
 };
 
+// pass both paddles into update function to verify contact
 var update = function() {
-
+	ball.update(player.paddle, computer.paddle);
 };
 
 var render = function() {
@@ -89,10 +90,50 @@ Ball.prototype.render = function() {
 	context.fill();
 };
 
+Ball.prototype.update = function(paddle1, paddle2) {
+	this.x += this.x_speed;
+	this.y += this.y_speed;
+	var top_x = this.x - 5;
+	var top_y = this.y - 5;
+	var bottom_x = this.x + 5;
+	var bottom_y = this.y + 5;
+
+	if(this.x - 5 < 0) { //represents hitting left side of screen
+		this.x = 5;
+		this.x_speed = -this.x_speed;
+	} else if(this.x + 5 > 400) { //hitting right side
+		this.x = 395;
+		this.x_speed = -this.x_speed;
+	}
+
+	if(this.y < 0 || this.y > 600) {//point was scored
+		this.x_speed = 0;
+		this.y_speed = 3;
+		this.x = 200;
+		this.y = 300;
+	}
+
+	if(top_y > 300) {
+		if(top_y < (paddle1 + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) { //hits player's paddle
+			this.y_speed = -3;
+			this.x_speed += (paddle1.x_speed / 2);
+			this.y += this.y_speed;
+		}
+	}	else {
+		if(top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
+			this.y_speed = 3;
+			this.x_speed += (paddle2.x_speed / 2);
+			this.y += this.y_speed;
+		}
+	}
+};
+
 // build objects, update render function
 var player = new Player();
 var computer = new Computer();
 var ball = new Ball(200, 300);
+
+// Animation! update update function and add update prototype to Ball object
 
 
 
